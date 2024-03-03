fc420b5ba456d22d37a4826da891663c44bc285c
deepali04 <nagwade.deepali@gmail.com>
2024-03-03 15:30:53 -0500

    improvement in css

--- src/components/experienceCard/ExperienceCard.js
+++ src/components/experienceCard/ExperienceCard.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import "./ExperienceCard.css";
-import { Fade, Flip } from "react-reveal";
+import { Fade } from "react-reveal";
 
 class ExperienceCard extends Component {
   render() {
